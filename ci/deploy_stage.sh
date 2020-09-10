999e5cf58524463065b70e7a2f1e5e2ce89ce2aa
HashTang <t@tsy6.com>
2020-09-11 01:31:05 +0800

    chore: 更新配置文件

--- ci/deploy_stage.sh
+++ ci/deploy_stage.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+ls
 npm install
 npm run build
 cd build
 tar -zcvf calendar.tar.gz *
-mv calendar.tar.gz /
+mv calendar.tar.gz /usr/share/nginx/hxkj/dist/demo/react-calendar/
+cd /usr/share/nginx/hxkj/dist/demo/react-calendar/
+tar -zxvf calendar.tar.gz
+rm -f calendar.tar.gz
