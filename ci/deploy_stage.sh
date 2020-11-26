778962fc4c585391509907652d3578cf72c3bb7a
HashTang <t@tsy6.com>
2020-11-26 23:50:14 +0800

    chore: update CI

--- ci/deploy_stage.sh
+++ ci/deploy_stage.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 npm install
 npm run build
-cd ../dist
+cd ./dist
+tar -zcvf calendar.tar.gz *
+rm -rf /usr/share/nginx/hxkj/dist/demo/react-calendar/*
+mv calendar.tar.gz /usr/share/nginx/hxkj/dist/demo/react-calendar/
+cd /usr/share/nginx/hxkj/dist/demo/react-calendar/
+tar -zxvf calendar.tar.gz
+rm -f calendar.tar.gz
 ls
-# tar -zcvf calendar.tar.gz *
-# mv calendar.tar.gz /usr/share/nginx/hxkj/dist/demo/react-calendar/
-# cd /usr/share/nginx/hxkj/dist/demo/react-calendar/
-# tar -zxvf calendar.tar.gz
-# rm -f calendar.tar.gz
